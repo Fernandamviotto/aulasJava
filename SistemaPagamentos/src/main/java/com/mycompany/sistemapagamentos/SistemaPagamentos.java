a0d1e10ce29f170b8b2d91ffde4d1999c8538382
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2024-09-03 20:23:09 -0300

    Base ex.02

---
+++ SistemaPagamentos/src/main/java/com/mycompany/sistemapagamentos/SistemaPagamentos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemapagamentos;
+
+/**
+ *
+ * @author fernanda.silva
+ */
+public class SistemaPagamentos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
